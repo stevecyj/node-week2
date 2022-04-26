c5555e4b1fd4589e892dbe7299e7a9bbd27fa341
stevecyj <ycsteven.tsao@gmail.com>
2022-04-26 10:46:13 +0800

    extract controllers

---
+++ controllers/posts.js
@@ -0,0 +1,32 @@
+const handleSuccess = require('../service/handleSuccess');
+const handleError = require('../service/handleError');
+const Posts = require('../model/posts');
+
+const posts = {
+  async getPosts({ req, res }) {
+    console.log(req);
+    const allPosts = await Posts.find();
+    handleSuccess(res, allPosts);
+    res.end();
+  },
+  async createPosts({ req, res, body }) {
+    try {
+      const data = JSON.parse(body);
+      if (data.content) {
+        const newPost = await Posts.create({
+          name: data.name,
+          content: data.content,
+          tags: data.tags,
+          type: data.type,
+        });
+        handleSuccess(res, newPost);
+      } else {
+        handleError(res);
+      }
+    } catch (err) {
+      handleError(res, err);
+    }
+  },
+};
+
+module.exports = posts;
--- routes/index.js
+++ routes/index.js
@@ -1,48 +1,22 @@
-const headers = require("../service/headers");
-const handleSuccess = require("../service/handleSuccess");
-const handleError = require("../service/handleError");
-const Posts = require("../model/posts");
+const HttpController = require('../controllers/http');
+const PostController = require('../controllers/posts');
 
 const routes = async (req, res) => {
-  let body = "";
-  req.on("data", (chunk) => {
+  console.log(req);
+  const { method, url } = req;
+  let body = '';
+  req.on('data', (chunk) => {
     body += chunk;
   });
-  if (req.url === "/posts" && req.method === "GET") {
-    const allPosts = await Posts.find();
-    handleSuccess(res, allPosts);
-    res.end();
-  } else if (req.url === "/posts" && req.method === "POST") {
-    req.on("end", async () => {
-      try {
-        const data = JSON.parse(body);
-        if (data.content) {
-          const newPost = await Posts.create({
-            name: data.name,
-            content: data.content,
-            tags: data.tags,
-            type: data.type,
-          });
-          handleSuccess(res, newPost);
-        } else {
-          handleError(res);
-        }
-      } catch (err) {
-        handleError(res, err);
-      }
-    });
-  } else if (req.method === "OPTIONS") {
-    res.writeHead(200, headers);
-    res.end();
+  if (url === '/posts' && method === 'GET') {
+    console.log(req);
+    PostController.getPosts({ req, res });
+  } else if (url === '/posts' && method === 'POST') {
+    req.on('end', () => PostController.createPosts({ req, res, body }));
+  } else if (method === 'OPTIONS') {
+    HttpController.cors(req, res);
   } else {
-    res.writeHead(404, headers);
-    res.write(
-      JSON.stringify({
-        status: "false",
-        message: "無此網站路由",
-      })
-    );
-    res.end();
+    HttpController.notFound(req, res);
   }
 };
 
