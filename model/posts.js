feafe943c7021005d19d68fe5969ddcc625fe9f5
stevecyj <ycsteven.tsao@gmail.com>
2022-04-27 15:38:56 +0800

    [schema]:remove version

--- model/posts.js
+++ model/posts.js
@@ -1,46 +1,48 @@
 const mongoose = require('mongoose');
-const postsSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, '貼文姓名未填寫']
-  },
-  tags: [
-    {
+const postsSchema = new mongoose.Schema(
+  {
+    name: {
       type: String,
-      required: [true, '貼文標籤 tags 未填寫']
-    }
-  ],
-  type: {
-    type: String,
-    enum:['group','person'],
-    required: [true, '貼文類型 type 未填寫']
+      required: [true, '貼文姓名未填寫'],
+    },
+    tags: [
+      {
+        type: String,
+        required: [true, '貼文標籤 tags 未填寫'],
+      },
+    ],
+    type: {
+      type: String,
+      enum: ['group', 'person'],
+      required: [true, '貼文類型 type 未填寫'],
+    },
+    image: {
+      type: String,
+      default: '',
+    },
+    createAt: {
+      type: Date,
+      default: Date.now,
+      select: false,
+    },
+    content: {
+      type: String,
+      required: [true, 'Content 未填寫'],
+    },
+    likes: {
+      type: Number,
+      default: 0,
+    },
+    comments: {
+      type: Number,
+      default: 0,
+    },
   },
-  image: {
-    type: String,
-    default: ""
-  },
-  createAt: {
-    type: Date,
-    default: Date.now,
-    select: false
-  },
-  content: {
-    type: String,
-    required: [true, 'Content 未填寫'],
-  },
-  likes: {
-    type: Number,
-    default: 0
-  },
-  comments:{
-    type: Number,
-    default: 0
-  },
-});
-
-const posts = mongoose.model(
-  'posts',
-  postsSchema
+  {
+    versionKey: false,
+  }
 );
+
+const posts = mongoose.model('posts', postsSchema);
 
 module.exports = posts;
