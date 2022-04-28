146315f96c6efc93edf63771ab5c5cf524be9f4f
stevecyj <ycsteven.tsao@gmail.com>
2022-04-28 08:58:15 +0800

    [service]:handleLocalDate,mod timezone

--- service/handleLocalDate.js
+++ service/handleLocalDate.js
@@ -1,6 +1,8 @@
 function handleLocal(v) {
   const d = new Date(v || Date.now());
-  d.setMinutes(d.getMinutes() - d.getTimezoneOffset());
+  d.setMinutes(d.getMinutes() - d.getTimezoneOffset()).toLocaleString('zh-TW', {
+    timeZone: 'Asia/Taipei',
+  });
   return d.toISOString();
 }
 
