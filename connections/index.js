3cb8dcc53ed28cda7c3a00c3c6a53ed2778a2fd4
stevecyj <ycsteven.tsao@gmail.com>
2022-05-05 05:54:11 +0800

    [fix]:catch error when DB connect fail

--- connections/index.js
+++ connections/index.js
@@ -1,10 +1,10 @@
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-dotenv.config({ path: "./config.env" });
-const DB = process.env.DATABASE.replace(
-  "<password>",
-  process.env.DATABASE_PASSWORD
-);
+dotenv.config({ path: './config.env' });
+const DB = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD);
 
-mongoose.connect(DB).then(() => console.log("資料庫連接成功"));
+mongoose
+  .connect(DB)
+  .then(() => console.log('資料庫連接成功'))
+  .catch((err) => console.log(`${err.codeName} 資料庫連接失敗`));
